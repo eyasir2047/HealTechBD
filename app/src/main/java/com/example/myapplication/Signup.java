418e9d81e593ab6f15f85fc701730e68939a5b64
eyasir2047 <eyasir2047@gmail.com>
2023-08-22 15:45:05 +0600

    sign up to login page

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
 
@@ -11,6 +14,14 @@ public class Signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        Button signupButton = (Button) findViewById(R.id.Signup);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this, MainActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
 
 
     }
